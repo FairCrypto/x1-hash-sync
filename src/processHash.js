05534b55ecf3f3ed4e40fd9bb93398deeea65b52
Leonid Belyaev <lbeliaev@gmail.com>
2023-12-08 16:13:55 -0800

    added retry block

--- src/processHash.js
+++ src/processHash.js
@@ -2,7 +2,7 @@ import assert from "assert";
 import {solidityPacked} from "ethers";
 import debug from "debug";
 
-const log = debug('hash-sync')
+const log = debug('hash-sync', {time: true});
 
 export const processHash = async (hash, contract) => {
   try {
