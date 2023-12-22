7d4c78309ae21095e8e68f8b215a78ddb45e3d55
Leonid Belyaev <lbeliaev@gmail.com>
2023-12-21 16:08:29 -0800

    filtering xunis

--- src/rx_server.js
+++ src/rx_server.js
@@ -62,7 +62,7 @@ subscribe = fromEvent(server, 'request')
           }),
           tap((data) => log('block', data)),
           bufferCount(Number(BATCH_SIZE)),
-          // tap((data) => log('batch', data)),
+          tap((data) => log('batch', data)),
           // mergeMap(data => from(processNewHashBatch(data, contract)))
         ),
         xunis.pipe(
