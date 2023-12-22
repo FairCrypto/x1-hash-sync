251a87dd9240cb7050a5e8b9fce3d9b58d065c9e
Leonid Belyaev <lbeliaev@gmail.com>
2023-12-21 18:57:35 -0800

    filtering xunis

--- src/processNewHashBatch.js
+++ src/processNewHashBatch.js
@@ -67,8 +67,8 @@ export const processHashBatch = async (hashes, contract, address) => {
         [[], [], []]
       );
 
-    const gas = await contract.bulkStoreRecords.estimateGas(address, params[1], params[2]);
-    const res = await contract.bulkStoreRecords(address, params[1], params[2], {
+    const gas = await contract.bulkStoreRecordBytes.estimateGas(address, params[1], params[2]);
+    const res = await contract.bulkStoreRecordBytes(address, params[1], params[2], {
       gasLimit: gas * 120n / 100n,
       maxFeePerGas: 10_000_000_000n,
       maxPriorityFeePerGas: 2_000_000_000n,
